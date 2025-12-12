377eafc0f2bf5aa62d0519e929eff6ef6442da79
Pădureanu Andreea <padureanumaria22@stud.ase.ro>
2025-12-12 21:25:21 +0200

    am rearanjat server.js

--- backend/server.js
+++ backend/server.js
@@ -4,33 +4,17 @@ const { sequelize } = require("./models");
 const inventoryRoutes = require("./routes/inventoryRoutes");
 const claimRoutes = require("./routes/claimRoutes");
 const integrationRoutes = require("./routes/integrationRoutes");
-
-const app = express();
-const port = 3000;
-
 const authRouter = require("./routes/authRouter")
 const productRouter = require("./routes/productRouter")
 const feedRouter = require("./routes/feedRouter")
 const groupRouter = require("./routes/groupRouter")
 const debugGroupRouter = require("./routes/debugGroupRouter")
 
+const app = express();
+const port = 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use('/inventory', inventoryRoutes);
-app.use('/claims', claimRoutes);
-app.use('/integrations', integrationRoutes);
-
-app.listen(port, () => {
-  console.log("The server is running on http://localhost:" + port);
-});
-
-// Middleware pentru erori
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ message: "Server Error" });
-});
-
 
 //aici pun base path
 app.use("/auth",authRouter)
@@ -38,7 +22,9 @@ app.use("/products", productRouter)
 app.use("/feed", feedRouter)
 app.use("/groups",groupRouter)
 app.use("/debug",debugGroupRouter)
-
+app.use('/inventory', inventoryRoutes);
+app.use('/claims', claimRoutes);
+app.use('/integrations', integrationRoutes);
 
 // Ruta de test și sync DB 
 app.get("/", async (req, res, next) => {
@@ -50,6 +36,7 @@ app.get("/", async (req, res, next) => {
   }
 });
 
+// Middleware pentru erori
 app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).json({ message: "Server Error" });
